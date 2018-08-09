4caccf62310a6c31fc05ae5084c58a67da6b0432
Taylor Doodchenko <taylordoodchenko@Taylors-MacBook-Pro.local>
2018-08-09 18:35:42 -0400

    images are now actionable. when page load you can select an opponent and the opponent image will be appended to the versus slot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,109 @@
 $(document).ready(function() {
+   
     
     
-    var theBoys = $("#theBoys");
-    var caveman = $("#caveman");
-    var Officer = $("#officer");
-    var cyrus = $("#cyrus");
-    var lahey = $("#lahey");
-    
-    
-    
-    var attackPoints;
-    var HP;
-    var AttackPower;
-    var counterAttack;
 
-    var playerHP = 0;
+   function start() {
+       var img1 = $("<img />")
+       img1.attr("src","./assets/images/CHOOSE.png")
+       img1.attr("id", "img1")
+    $("#select-opp").html(img1);
+   }
+
+   start();
+
+
+    // attack from the boys will be link to 'attack' button
+    var theBoys = 
+        {
+        "id"        : "theBoys",
+        "name"      : "The Boys",
+        "hp"        : 120,
+        "damage"    : 14,
+        "dmgInc"    : 10,
+        }
+
+    // when one of the below opponents are selected the image will be placed in the div with id="select-opp"
+
+
+     var caveman =
+        {
+          "id"      : "1",
+          "name"    : "Sam 'caveman' Losco",
+          "imgSrc"  : '<img src="./assets/images/caveman.jpg" class="img-thumbnail" >'
+        };
+    var officer =
+        {
+          "id"      : "2",
+          "name"    : "Officer George Green",
+          "imgSrc"  : '<img src="./assets/images/officer-george-green.jpg" class="img-thumbnail" >'
+        };
+    var cyrus = 
+        {
+          "id"      : "3",
+          "name"    : "Cyrus, Terry & Dennis",
+          "imgSrc"  : '<img src="./assets/images/cyrus-twins.jpg" class="img-thumbnail" >'
+        };
+    var lahey =
+        {
+          "id"      : "4",
+          "name"    : "Jim Lahey & Randy Bobandy",
+          "HP"      : 0,
+          "imgSrc"  : '<img src="./assets/images/lahey-randy.jpg" class="img-thumbnail" >'
+        };
+
+    var charArray = [caveman, officer, cyrus, lahey];
+
     var wins = 0;
     var losses = 0;
+    var OpponentChosen = false;
 
     $('#wins').text(wins);
     $('#losses').text(losses);
 
-    $("#theBoys").on("click", function() {
-        console.log("Ricky, Julian and Bubbles are good to go!");    
-       
+   $('.opp-btn').on("click", function() {
+    console.log("it is working");
+    console.log($(this).attr("id"));
+    var charID = $(this).attr("id");
+    console.log(charArray[0].id)
+    for (var i=0; i<charArray.length; i++) {
+     if (charID === charArray[i].id) {
+        console.log(charArray[i].imgSrc);
+        $("#select-opp").replaceWith(charArray[i].imgSrc);
+        console.log(charArray[i].name);
+        $("#opp-name").replaceWith(charArray[i].name);
+        
 
-    });
-
-
-    $("#caveman").on("click", function() {
-
-        $('#select-opp').append("<img src=assets/images/caveman.jpg>");
-
-
-    })   
-   
-
-
-    })
+     }
+    }
     
-    
-    /////////////////////
-    //////
-    //////
-    /////
-    ////
+
+   })
+
+
+
+
+    // function reset() {
+    //     OpponentChosen = false;
+    //     wins = 0;
+    //     losses = 0;
+    //     $("#select-opp").append('<img src="./assets/images/CHOOSE.png" width="300px"/>');
+    // }
+
+    // if ($("#1").click) {
+    //     $("#select-opp").replaceWith('<img src="./assets/images/caveman.jpg" width="300px" class="img-thumbnail" />');
+    //     console.log("selected opponent")
+    // }
+
+
+
+  })
+
+
+  
+    // var attackPoints;
+    // var HP;
+    // var AttackPower;
+    // var counterAttack;
 
    
-    // characters = [
-    //     {
-    //       "id"      : "caveman",
-    //       "name"    : "Sam 'caveman' Losco",
-    //       "hp"      : 100,
-    //       "damage"  : 12,
-    //       "dmgInc"  : 12,
-    //       "avatar"  : "./assets/images/caveman.jpg"
-    //     },
-    //     {
-    //       "id"      : "officer",
-    //       "name"    : "Officer George Green",
-    //       "hp"      : 110,
-    //       "damage"  : 10,
-    //       "dmgInc"  : 8,
-    //       "avatar"  : "./assets/images/officer-george-green.jpg"
-    //     },
-    //     {
-    //       "id"      : "cyrus",
-    //       "name"    : "Cyrus, Terry & Dennis",
-    //       "hp"      : 130,
-    //       "damage"  : 11,
-    //       "dmgInc"  : 8,
-    //       "avatar"  : "./assets/images/cyrus-twins.jpg"
-    //     },
-    //     {
-    //       "id"      : "lahey",
-    //       "name"    : "Jim Lahey & Randy Bobandy",
-    //       "hp"      : 180,
-    //       "damage"  : 18,
-    //       "dmgInc"  : 5,
-    //       "avatar"  : "./assets/images/lahey-randy.jpg"
-    //     }
-    //     ];  
-
-
-
